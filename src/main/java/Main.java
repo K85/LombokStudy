135d0414d8c0dac711058d2006ce29b162092d5c
HakuzosuSAMA <3172906506@qq.com>
2021-06-18 22:31:44 +0800

    LOMBOK STUDY END

---
+++ src/main/java/Main.java
@@ -0,0 +1,26 @@
+import bean.User;
+import lombok.extern.log4j.Log4j2;
+
+
+public class Main {
+
+
+    public void test() {
+
+
+
+    }
+
+
+    public static void main(String[] args) {
+
+        User user = new User();
+
+
+
+
+
+
+    }
+
+}
--- src/main/java/bean/User.java
+++ src/main/java/bean/User.java
@@ -1,8 +1,18 @@
 package bean;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.experimental.Accessors;
+
 
 @Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Accessors(chain = true)
 public class User {
     private String name;
+    private int id;
+
 }
